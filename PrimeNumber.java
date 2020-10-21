c1e236f41a532d20fa2590939a27cfc761cc525b
Xavi Gómez Canals <jumpering@gmail.com>
2020-10-21 17:03:15 +0200

    method isPrime changed to static

--- PrimeNumber.java
+++ PrimeNumber.java
@@ -6,7 +6,7 @@ public class PrimeNumber {
         this.number = 2;
     }
 
-    public boolean isPrime (int number) {
+    public static boolean isPrime (int number) {
         for (int i = 2; i < number; i++) {
             if (number % i == 0) {
                 return false;
